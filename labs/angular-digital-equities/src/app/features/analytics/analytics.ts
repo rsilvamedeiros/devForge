9a12d631761a476a07473a4906b198c1ea4017aa
Renan Medeiros <renanmedeiros79@gmail.com>
2026-09-22 23:43:00 -0300

    feat: refocus angular lab on academic learning

--- labs/angular-digital-equities/src/app/features/analytics/analytics.ts
+++
@@ -1,7 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-
-@Component({ selector: 'app-analytics', imports: [MatIconModule], templateUrl: './analytics.html', styleUrl: './analytics.scss', changeDetection: ChangeDetectionStrategy.OnPush })
-export class Analytics {
-  readonly heatmap = [1,2,3,1,0,2,4,3,2,1,3,4,2,0,1,3,4,4,2,1,0,2,3,4,3,2,1,2];
-}
